a42f485be97b99c0deae5ddb31cea6e6814367a6
lcnorine <60401135+lcnorine@users.noreply.github.com>
2020-03-30 12:47:38 -0700

    inherit top ten list from idirectionable

--- TopTenList.h
+++ TopTenList.h
@@ -4,17 +4,20 @@
 #include <string>
 #include <vector>
 #include "Hyperlink.h"
+#include "Idirectionable.h"
 
 using std::string;
 using std::vector;
 
-class TopTenList
+class TopTenList : IDirectionable
 {
   private:
     vector _list;
   public:
     void set_at(int index, Hyperlink link);
     Hyperlink get(int index);
+    void display_forward();
+    void display_backward();
 };
 
 #endif
